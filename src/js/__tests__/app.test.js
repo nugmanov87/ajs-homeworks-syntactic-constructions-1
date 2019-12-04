85e9d07f802eb5777f00e73698b7168b5b1e5dc7
Ruslan Nugmanov <ruslan-nugmanov87@mail.ru>
2019-12-04 10:52:51 +0500

    2 версия 1 задание, не исправлены все ошибки

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,13 +1,13 @@
-import converter from "../app";
+import converter from '../app';
 
-test("True integer", () => {
+test('True integer', () => {
   expect(converter(10)).toBe(10);
 });
 
-test("Integer by string", () => {
-  expect(converter("10")).toBe(10);
+test('Integer by string', () => {
+  expect(converter('10')).toBe(10);
 });
 
-test("Not integer", () => {
-  expect(() => converter("341.567")).toThrowError("341.567 не является числом");
+test('Not integer', () => {
+  expect(() => converter('341.567')).toThrowError('341.567 не является числом');
 });
